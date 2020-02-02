31d6cdfd07de58a81a06b534779983b96a6b3919
Tom McLean <thomasowenmclean@gmail.com>
2020-02-02 18:43:48 +0000

    Edited new tests to pass

--- tests/deser.rs
+++ tests/deser.rs
@@ -18,16 +18,47 @@ fn deser_same(src: &[u8]) {
 	let output = p.wait_with_output()
 		.expect("Output err");
 	let json = String::from_utf8_lossy(&output.stdout);
-    println!("{}", json);
 	let pandoc: Pandoc = serde_json::from_str(&json)
 		.expect("Error deserializing json");
 	let ser = serde_json::to_string(&pandoc)
 		.expect("Error serializing");
-	assert_eq!(json, ser);
+
+	let mut cmd = Command::new("pandoc")
+		.args(&["-f", "json", "-t", "json"])
+		.stdin(Stdio::piped())
+		.stdout(Stdio::piped())
+		.spawn()
+		.expect("Error spawning pandoc");
+	{
+		let stdin = cmd.stdin.as_mut()
+    		.expect("Failed to open stdin");
+    	stdin.write_all(ser.as_bytes())
+    		.expect("Failed to write to stdin");
+	}
+	let ser_output = cmd.wait_with_output()
+		.expect("Output err");
+	
+	let mut cmd = Command::new("pandoc")
+		.args(&["-f", "json", "-t", "json"])
+		.stdin(Stdio::piped())
+		.stdout(Stdio::piped())
+		.spawn()
+		.expect("Error spawning pandoc");
+	{
+		let stdin = cmd.stdin.as_mut()
+    		.expect("Failed to open stdin");
+    	stdin.write_all(json.as_bytes())
+    		.expect("Failed to write to stdin");
+	}
+	let orig_output = cmd.wait_with_output()
+		.expect("Output err");
+
+
+	assert_eq!(orig_output, ser_output);
 }
 
 #[test]
-fn it_works() {
+fn deserializing_roundtrip() {
 	let t1 = include_bytes!("testsuite.txt");
 	let t2 = include_bytes!("tables.txt");
 	let t3 = include_bytes!("markdown-reader-more.txt");
